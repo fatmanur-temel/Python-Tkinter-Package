8fee3ace877f40fc48711596ffba9bef1e5ccc13
Fatma Nur <fatmanurtml@gmail.com>
2022-02-21 16:09:44 +0300

    Create window.py

---
+++ window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window=Tk()
+window.title("Window")
+window.geometry("500x300+500+200")
+
+label=Label(text="Merhaba",font="Verdana 22 bold")
+label.pack()
+
+window.mainloop()
